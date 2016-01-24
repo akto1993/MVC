5283d5da5c30a9b47dbee21b7fed15361bd4c159
akto1993 <akto1993@gmail.com>
2016-01-24 16:53:59 +0100

    10. Adding Validation Done
    po zmianach walidacji należy wykonać komendy aby zauktualizować bazę
    danych:
    
    add-migration DataAnnotations
    update-database

--- Main/MVC/MVC/Models/Game.cs
+++ Main/MVC/MVC/Models/Game.cs
@@ -8,14 +8,29 @@ namespace MVC.Models
     public class Game
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 300)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [Range(1, 100)]
         public int Done { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(4)]
         public string Platform { get; set; }
     }
 
